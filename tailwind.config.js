08d306c0f1abbe7db4db1e4d0d0feb9567f94552
Godfrey F <godfern13@gmail.com>
2025-07-07 01:16:28 +0530

    Fix Google Fonts SSL issue by using system fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        geist: ['Geist', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
